3f4f42a1e659eefcd137f9b4183eec6f29eb98f3
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-01-30 09:25:04 +0530

    User Defined Function Demos

---
+++ 3_Programs/2024/PS1UDF11.C
@@ -0,0 +1,32 @@
+//TITLE : WAP to CREATE Usere defined function demo
+// of type no argument no return type
+
+#include <stdio.h>
+#include <conio.h>
+// 1. function declaration
+void amroliwel();
+
+void main()
+{
+clrscr();
+// 3. function call
+amroliwel();
+
+amroliwel();
+
+
+getch();
+}
+// 2. function definition
+void amroliwel()
+{
+  printf("Welcome to Amroli FYBCA Div 10 ::\n");
+}
+
+
+
+
+
+
+
+
---
+++ 3_Programs/2024/PS1UDF44.C
@@ -0,0 +1,34 @@
+//TITLE : WAP to CREATE Usere defined function demo
+// of type with argument and return type
+
+#include <stdio.h>
+#include <conio.h>
+// 1. function declaration
+int amroli(int a , int b);
+
+void main()
+{
+   int op;
+   clrscr();
+   // 3. function call
+   op = amroli(4,5);
+  printf("Your answer is %d :  ",op);
+   getch();
+
+}
+// 2. function defination
+int amroli(int a, int b)
+{
+  //int ans;
+  printf("FUNCTION STARTS HERE.....");
+ // ans = a+ b;
+  return  a + b;
+}
+
+
+
+
+
+
+
+

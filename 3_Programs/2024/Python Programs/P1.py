084ca16741e618cacc882031c79e27538be5f50e
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-02-08 11:11:03 +0530

    Python Programs

---
+++ 3_Programs/2024/Python Programs/P1.py
@@ -0,0 +1,19 @@
+#HELLO FROM AMROLI
+print("AMROLI BCA COLLEGE")
+x=5     #sample value of x
+y = "AMROLI"
+print(x+5)
+print(y)
+''' THIS IS MULTI LINE COMMENT
+RUN YOUR PROGRAM USING F5
+'''
+a=111
+print(a)
+print("a")
+
+
+
+
+
+
+
---
+++ 3_Programs/2024/Python Programs/prgwhenitwaswednesdayforfybcadiv9.py
@@ -0,0 +1,20 @@
+x = """
+Program for Demo of UDF 
+def amroli(name= 'SUTEX'):
+    print("HELLO FROM FYBCA to " + name)
+    return 'BYE'
+ 
+print("WELCOME ")
+print(amroli('HITESH'))
+Output:
+"""
+print(x)
+def amroli(name= 'SUTEX'):
+    print("HELLO FROM FYBCA to " + name)
+    return 'BYE'
+ 
+print("WELCOME ")
+print(amroli('HITESH'))
+
+
+

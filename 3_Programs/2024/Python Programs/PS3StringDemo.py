6a2128ade42541423934b12aa78b40b5ab6f0602
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-02-08 12:58:33 +0530

    Upto Div 10

---
+++ 3_Programs/2024/Python Programs/PS3StringDemo.py
@@ -0,0 +1,32 @@
+#WAP to show demo of Multi Line String
+prog ="""#WAP to show demo of Multi Line String
+
+x = 'R V PATEL'
+y= 'SUTEX BCA'
+z = 'PROF. VB SHAH'
+
+a =  
+R V PATEL
+SUTEX BANK
+PROF VB SHAH
+print(x)
+print(y)
+print(z)
+print(a)
+OUTPUT : 
+"""
+print(prog)
+x = 'R V PATEL'
+y= 'SUTEX BCA'
+z = 'PROF. VB SHAH'
+
+a =  """
+R V PATEL
+SUTEX BANK
+PROF VB SHAH"""
+print(x)
+print(y)
+print(z)
+print(a)
+
+

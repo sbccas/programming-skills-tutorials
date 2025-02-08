f12b50829b5199827d3c5a69db6c45a6daeb67d7
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-02-08 12:03:20 +0530

    Updated 8 Feb

--- 3_Programs/2024/Python Programs/PS2CastingDemo.py
+++ 3_Programs/2024/Python Programs/PS2CastingDemo.py
@@ -1,15 +1,14 @@
-#HELLO FROM AMROLI
+#WAP for Showing Casting Demo
 print("AMROLI BCA COLLEGE")
 
 x=5     #sample value of x
 y = "AMROLI"
 print(x+5)
+#print(y +5)
 print(y+str(5))
 ''' THIS IS MULTI LINE COMMENT
 RUN YOUR PROGRAM USING F5
 '''
-a=111
-print(a)
-print("a")
+
 
 

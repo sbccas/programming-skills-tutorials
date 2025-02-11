ad50823921049171881d648a9d31fc2e066d3222
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-02-11 09:45:43 +0530

    PS3StringMethods

---
+++ 3_Programs/2024/Python Programs/PS3StringMethods.py
@@ -0,0 +1,54 @@
+#WAP to Show Demo of String Methods in Python
+x="SUTEX BANK COLLEGE OF COMPUTER APPLICATIONS AND SCIENCE"
+k="AMROLI"
+#CENTER METHOD
+print('CENTER METHOD DEMO'.center(58,))
+
+print(k.center(10,'$'))
+print(k.center(20,"!"))
+#COUNT METHOD
+print('COUNT METHOD DEMO'.center(58,))
+print(x.count('BANK'))
+print(x.count("OF"))
+#JOIN METHOD
+print('JOIN METHOD DEMO'.center(58,))
+y = "AMAR", "AKBAR", "ANTHONY"
+ans = "?".join(y)
+print(ans)
+#LEN METHOD
+print('LEN METHOD ON X'.center(58,))
+print(len(x))
+print(len(k))
+print(len(y))
+print(len(ans))
+#MAX METHOD
+print('MAX METHOD'.center(58,))
+print(max(k))
+#MIN METHOD
+print('MIX METHOD'.center(58,))
+print(min(k))
+
+#REPLACE METHOD
+print('REPLACE METHOD'.center(58,))
+x = x.replace('COMPUTER','AI')
+print(x)
+#LOWER AND UPPER METHOD
+print('LOWER AND UPPER METHOD'.center(58,))
+print(x.lower())
+print(x.upper())
+#COUNT TOTAL CHARACTER IN SINGLE LINE OUTPUT
+test = 'A'
+print(test.center(59,'$'))
+
+
+
+
+
+
+
+
+
+
+
+
+

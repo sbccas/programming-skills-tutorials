edf4f43d5039aabde4d8de46f6d477e8df57bca7
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-02-15 12:57:14 +0530

    Upto Unit 4

---
+++ 3_Programs/2024/Python Programs/PS4ContitionStatementDemo.py
@@ -0,0 +1,45 @@
+#WAP to show demo of COnditional Statements
+"""
+a= 10
+b= 10
+if a<b:
+    print("A is less than B")
+elif a==b:
+    print("Both are Same")
+else:
+    print("B is less than A")
+
+    """
+a= -100
+if a>0:
+    print("A is greater than 0")
+    if a>10:
+        print("A is greater than 10")
+        if a > 20:
+            print("A is greater than 20")
+            if a>30:
+                print("A is greater than 30")
+                if a>40:
+                    print("A is greater than 40")
+                    if a> 50:
+                        print("A is greater than 50")
+                    else:
+                        print("A between 40 and 50")
+                else:
+                    print("A between 30 and 40")
+            else:
+                print("A between 20 and 30")
+        else:
+            print("A between 10 and 20")
+    else:
+        print("A between 0 and 10")
+else:
+    print("A is less than 0")
+    
+        
+
+                    
+
+
+
+

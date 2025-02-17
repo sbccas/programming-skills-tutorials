5b5cdcdabfa458b243f560f44b0cca592a0fe361
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-02-17 09:05:33 +0530

    FOR LOOP DEMO

---
+++ 3_Programs/2024/Python Programs/PS4IterartiveProgramsPart2.py
@@ -0,0 +1,47 @@
+#WAP to show demo of ITERATIVE STATEMENTS
+
+#FOR LOOP STATEMENT
+x= "AMROLI"
+for i in x:
+    print(i)
+#printing from list
+y=["BCA","BBA","BCOM","BSC DSA","BSC IT","MSC IT"]
+for j in y:
+    print(j)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

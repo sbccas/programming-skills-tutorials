1a0f78c86fa7d50e01719266bfc9c47bf649bc30
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-02-17 09:30:45 +0530

    Upto Nested For Loop

--- 3_Programs/2024/Python Programs/PS4IterartiveProgramsPart2.py
+++ 3_Programs/2024/Python Programs/PS4IterartiveProgramsPart2.py
@@ -1,5 +1,5 @@
 #WAP to show demo of ITERATIVE STATEMENTS
-
+"""
 #FOR LOOP STATEMENT
 x= "AMROLI"
 for i in x:
@@ -8,6 +8,79 @@ for i in x:
 y=["BCA","BBA","BCOM","BSC DSA","BSC IT","MSC IT"]
 for j in y:
     print(j)
+#Range FUNCTION
+for x in range(10):
+    print(x) #0,1,2,3,4,5,6,7,8,9
+#Range 2
+for y in range(22,27):
+    print(y) #22,23,24,25,26
+#Range 3
+for z in range(30,40,3):
+    print(z)  #30,33,36,39
+
+#ELSE IN FOR LOOP
+
+for x in range(10):
+    print(x)
+else:
+    print("LOOP COMPLETED SUCCESSFULLY")
+#ELSE IN FOR LOOP with break statement
+for y in range(10):
+    print(y)
+    if(y==4):
+        break    
+else:
+    print("LOOP COMPLETED SUCCESSFULLY")
+"""
+#NESTED FOR  LOOP DEMO
+
+name = ["AMROLI", "SUTEX"]
+course = ["BCA","BBA","BCOM","BSC DSA","BSC IT","MSC IT"]
+
+for x in name:
+    for y in course:
+        print(x,y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
     

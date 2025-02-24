5a8c8120890974811038bec9a9642df08b682046
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-02-24 09:29:18 +0530

    Program for Input Demo

---
+++ 3_Programs/2024/Python Programs/PS4InputDemo.py
@@ -0,0 +1,46 @@
+# WAP TO SHOW DEMO OF INPUT FUNCTION
+"""
+i=int( input("ENTER VALUE OF I"))
+j=int( input("ENTER VALUE OF J"))
+ans = i+j
+print("YOUR ANSWER IS : "+ str(ans))
+"""
+
+# WAP TO CREATE A LIST AND DISPLAY IT
+x=[]
+size= int(input("PLEASE ENTER SIZE OF LIST : "))
+for i in range(size):
+    x.append(input("ENTER NAME : "))
+
+print("YOUR LIST IS READY:")
+print("SIZE OF LIST :" + str(len(x)))
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

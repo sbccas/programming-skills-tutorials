ca30f3d6e8756fbc40b6f6bc7dcfd8572fae135a
HITESH PATEL <115479871+contact2hitech@users.noreply.github.com>
2025-03-20 13:50:16 +0530

    Unit 5 Demo Programs

---
+++ 3_Programs/2024/Python Programs/PS5TupleDemo.py
@@ -0,0 +1,13 @@
+# WAP TO SHOW DEMO OF TUPLE IN PYTHON
+
+
+t1 =("AMROLI","BCA","SUTEX","BCOM","BBA")
+print(t1)
+
+for i in t1:
+    print(i)
+
+print(type(t1))
+''' t2'''
+t2 =   tuple(["AMROLI","BCA"])
+print(type(t2))
